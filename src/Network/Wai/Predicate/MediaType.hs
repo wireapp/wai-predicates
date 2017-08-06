00a533c2365cc11cc4dd9b661eae3a8f9991190f
Toralf Wittner <tw@dtex.org>
2017-08-06 17:58:51 +0200

    Support singletons library versions >= 2.3

--- src/Network/Wai/Predicate/MediaType.hs
+++ src/Network/Wai/Predicate/MediaType.hs
@@ -1,12 +1,18 @@
+{-# LANGUAGE CPP            #-}
 {-# LANGUAGE DataKinds      #-}
 {-# LANGUAGE KindSignatures #-}
 
 module Network.Wai.Predicate.MediaType where
 
 import Data.ByteString (ByteString)
-import Data.ByteString.Char8 (pack)
 import Data.Singletons
 import Data.Singletons.TypeLits
+
+#if MIN_VERSION_singletons(2,3,0)
+import Data.Text.Encoding (encodeUtf8)
+#else
+import Data.ByteString.Char8 (pack)
+#endif
 
 data Media (t :: Symbol) (s :: Symbol) = Media
     { rawType      :: !ByteString
@@ -15,14 +21,22 @@ data Media (t :: Symbol) (s :: Symbol) = Media
     , mediaParams  :: ![(ByteString, ByteString)]
     } deriving (Eq, Show)
 
-mediaType :: SingI t => Media t s -> ByteString
+mediaType :: KnownSymbol t => Media t s -> ByteString
 mediaType m = withSing (f m)
   where
     f :: Media t s -> Sing t -> ByteString
+#if MIN_VERSION_singletons(2,3,0)
+    f _ t = encodeUtf8 (fromSing t)
+#else
     f _ t = pack (fromSing t)
+#endif
 
-mediaSubType :: SingI s => Media t s -> ByteString
+mediaSubType :: KnownSymbol s => Media t s -> ByteString
 mediaSubType m = withSing (f m)
   where
     f :: Media t s -> Sing s -> ByteString
+#if MIN_VERSION_singletons(2,3,0)
+    f _ s = encodeUtf8 (fromSing s)
+#else
     f _ s = pack (fromSing s)
+#endif
